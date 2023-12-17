ebc23851566a3ea0ed8c818e1c3bb5775d1c1837
owolfdev <owolfdev@gmail.com>
2023-12-18 04:16:23 +0700

    update incremental

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,16 @@ import AbstractArt from "@/components/graphics/abstract-image";
 export default function Home() {
   return (
     <div className="flex flex-col gap-8 justify-center align-middle items-center">
-      <h1 className="text-3xl sm:text-5xl font-bold text-center">
+      {/* <h1 className="text-3xl sm:text-5xl font-bold text-center">
         Welcome to <span className="gradient-text">MDX</span> Blog
-      </h1>
-      <p className="text-center">
+      </h1> */}
+      {/* <p className="text-center">
         This is a simple blog built with Next.js and MDX.
-      </p>
-      <LoaderLink isButton={true} url="/blog">
+      </p> */}
+      {/* <LoaderLink isButton={true} url="/blog">
         Start Reading
-      </LoaderLink>
-      <AbstractArt />
+      </LoaderLink> */}
+      {/* <AbstractArt /> */}
     </div>
   );
 }
