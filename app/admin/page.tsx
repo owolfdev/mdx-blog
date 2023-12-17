819b179f60fe6d0ee490d4dd98783256ef1a6687
owolfdev <owolfdev@gmail.com>
2023-12-18 04:42:33 +0700

    update incremental

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -6,9 +6,7 @@ export default function Admin() {
       <h1 className="text-4xl sm:text-5xl font-bold text-center">
         Administration
       </h1>
-      <div className="flex flex-col gap-4">
-        <CachePostsButton />
-      </div>
+      <div className="flex flex-col gap-4">{/* <CachePostsButton /> */}</div>
     </div>
   );
 }
--- app/contact/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Contact() {
-  return (
-    <div className="flex flex-col gap-8">
-      <h1 className="text-4xl sm:text-5xl font-bold text-center">Contact</h1>
-    </div>
-  );
-}
