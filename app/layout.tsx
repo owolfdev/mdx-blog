7900ef2beae76996ae78fd4e87b09d4e83d5c425
owolfdev <owolfdev@gmail.com>
2023-12-18 04:25:54 +0700

    update incremental

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,20 +29,20 @@ export default function RootLayout({
     >
       <html lang="en">
         <body className={inter.className}>
-          <ThemeProvider
+          {/* <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
-          >
-            {/* <SiteHeader /> */}
-            <main className="flex flex-col items-center  min-h-[calc(100vh-200px)] pt-12 pb-12 px-6 sm:px-0 sm:pt-20 sm:pb-20">
-              {/* <div className="text-lg sm:text-base max-w-xl w-full mx-auto">
+          > */}
+          {/* <SiteHeader /> */}
+          <main className="flex flex-col items-center  min-h-[calc(100vh-200px)] pt-12 pb-12 px-6 sm:px-0 sm:pt-20 sm:pb-20">
+            {/* <div className="text-lg sm:text-base max-w-xl w-full mx-auto">
                 {children}
               </div> */}
-            </main>
-            {/* <Footer /> */}
-          </ThemeProvider>
+          </main>
+          {/* <Footer /> */}
+          {/* </ThemeProvider> */}
         </body>
       </html>
     </ClerkProvider>
