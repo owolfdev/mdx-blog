0b2ec7e230300c7fd1666c5ce15ef428068ff590
owolfdev <owolfdev@gmail.com>
2023-12-22 07:37:16 +0700

    update posts

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-8 justify-center align-middle items-center">
       <h1 className="text-3xl sm:text-5xl font-bold text-center">
-        Welcome to <span className="gradient-text">MDX</span> Blog
+        Welcome to <span className="gradient-text">MDX</span>Blog
       </h1>
       <p className="text-center">
         This is a simple blog built with Next.js and MDX.
@@ -22,13 +22,19 @@ export default function Home() {
 }
 
 const heroContent = `
-**MDX Blog** offers a simple, yet powerful template for creating blogs using **MDX** (Markdown + JSX) and **Next.js 14**.
+**MDXBlog** offers a simple, yet powerful template for creating blogs using **MDX** (Markdown + JSX) and **Next.js 14**.
 
 **Why MDX?** MDX blends Markdown's straightforward syntax with the capability to embed dynamic JSX elements. Perfect for interactive, rich-content blogs.
 
-**Get MDX Blog:** Download the [**repo**](https://github.com/owolfdev/mdx-blog). Instructions for installation and deployment are included in the README.
+**Get MDXBlog:** Download the [**repo**](https://github.com/owolfdev/mdx-blog). Instructions for installation and deployment are included in the README.
 
-**Learn More About MDX**:
+**Learn More About MDXBlog:**
+
+- • [MDXBlog Official Documentation. AKA the 'about' page.](/about)
+- • [MDXBlog GitHub Repo](https://github.com/owolfdev/mdx-blog)
+- • [The Blog, where you can find the latest updates](/blog)
+
+**Learn More About MDX format**:
 - • [MDX Official Documentation](https://mdxjs.com/)
 - • [Integrating MDX with Next.js](https://nextjs.org/docs)
 `;
