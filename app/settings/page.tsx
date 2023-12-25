3c8c5496c78e1d8c75521002826ff3329c80e0dd
owolfdev <owolfdev@gmail.com>
2023-12-25 10:09:32 +0700

    update admin and settings

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,11 +1,7 @@
 import CachePostsButton from "@/components/admin/cache-posts-button";
 import OpenCategoriesInVSCode from "./open-categories-in-vs-code";
-<<<<<<< HEAD
-import OpenAdmin from "./open-admin";
-=======
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
->>>>>>> data-table
 
 export default function Settings() {
   return (
@@ -13,17 +9,10 @@ export default function Settings() {
       <h1 className="text-4xl sm:text-5xl font-bold text-center">Settings</h1>
       <div className="flex flex-col gap-4">
         <div className="flex flex-col gap-2">
-<<<<<<< HEAD
-          <OpenAdmin />
-          <p className="text-sm text-muted-foreground">
-            The Admin page is used to manage content.
-          </p>
-=======
           <Link href="/admin">
             <Button>Administration</Button>
           </Link>
           <p className="text-sm text-muted-foreground">Manage posts data.</p>
->>>>>>> data-table
         </div>
         <div className="flex flex-col gap-2">
           <CachePostsButton />
