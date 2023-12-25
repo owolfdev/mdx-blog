fd9e889ff4799ab191e0e22d4183bff634b734f8
owolfdev <owolfdev@gmail.com>
2023-12-26 05:42:44 +0700

    updates

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,7 @@ import { ContactForm } from "./form";
 function Contact() {
   return (
     <>
-      <div className="flex flex-col gap-8 w-full ">
+      <div className="flex flex-col gap-8 w-full">
         <h1 className="text-4xl sm:text-5xl font-bold text-center">Contact</h1>
         <div>
           <ContactForm />
