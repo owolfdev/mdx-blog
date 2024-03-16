35a6b6c591e595af7ae41fe153064c5150d64e27
owolfdev <owolfdev@gmail.com>
2024-03-16 08:31:55 +0700

    update main page and contact messages page

--- components/nav/footer.tsx
+++ components/nav/footer.tsx
@@ -5,7 +5,13 @@ export function Footer() {
     <footer className=" bottom-0 z-40 w-full border-t bg-background p-6 ">
       <div className="sm:px-8 px-4 flex flex-col justify-between items-center h-16 space-y-4 sm:space-y-0">
         <div className="flex gap-6 items-center">
-          <div className="">&copy; {new Date().getFullYear()} MDX Blog</div>
+          <div className="">
+            &copy; {new Date().getFullYear()}{" "}
+            <span className="font-bold">MDX Blog</span> created by{" "}
+            <Link target="_blank" href="https://owolf.com">
+              OWolf
+            </Link>
+          </div>
         </div>
         <nav className="flex gap-4 items-center text-sm">
           <Link href="/">Home</Link>
