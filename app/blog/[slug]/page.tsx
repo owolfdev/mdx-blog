513ff9e24d8e82320c92eda0e0729dd19d4932ff
owolfdev <owolfdev@gmail.com>
2024-04-07 09:16:44 +0700

    update width

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -101,7 +101,7 @@ export default async function BlogPage({
   };
 
   return (
-    <div className="flex flex-col gap-3 sm:max-w-xl max-w-xs">
+    <div className="flex flex-col gap-3 sm:max-w-2xl max-w-xs">
       <div className="mb-2">
         <h1 className="text-5xl font-bold mb-2">{props.frontMatter.title}</h1>
         <div>{props.frontMatter.date}</div>
