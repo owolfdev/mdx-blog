0ca39edc8078ae3c957b0bd46f674b7d8a3843cf
owolfdev <owolfdev@gmail.com>
2024-11-10 08:54:31 +0700

    update next sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,28 +1,21 @@
-// next-sitemap.js
+/** @type {import('next-sitemap').IConfig} */
+
 module.exports = {
-  siteUrl: "https://www.mdxblog.io", // Replace with your site URL
-  generateRobotsTxt: true, // Optional, but recommended for SEO
-  sitemapSize: 7000, // The maximum number of URLs per sitemap file
+  siteUrl: "https://www.mdxblog.io",
+  generateRobotsTxt: true, // (optional)
+  exclude: [
+    "/blog/.DS_Store",
+    "/blog/README.md",
+    "/blog/create",
+    "/apple-icon.png",
+    "/settings",
+    "/contact",
+    "/contact/thank-you",
+    "/privacy",
+    // "/api/get-contact-messages", // Exclude API page
+    // "/api/get-posts", // Exclude API page
+    "/contact/messages", // Exclude specific route
+    "/profile", // Exclude specific route
+  ],
+  // ...other options
 };
-
-// /** @type {import('next-sitemap').IConfig} */
-
-// module.exports = {
-//   siteUrl: "https://www.mdxblog.io",
-//   generateRobotsTxt: true, // (optional)
-//   exclude: [
-//     "/blog/.DS_Store",
-//     "/blog/README.md",
-//     "/blog/create",
-//     "/apple-icon.png",
-//     "/settings",
-//     "/contact",
-//     "/contact/thank-you",
-//     "/privacy",
-//     // "/api/get-contact-messages", // Exclude API page
-//     // "/api/get-posts", // Exclude API page
-//     "/contact/messages", // Exclude specific route
-//     "/profile", // Exclude specific route
-//   ],
-//   // ...other options
-// };
