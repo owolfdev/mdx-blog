157998143fcd4bab19df6feb1979d9121b2deba5
owolfdev <owolfdev@gmail.com>
2024-11-10 08:28:42 +0700

    add next sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: "https://www.mdxblog.io",
+  generateRobotsTxt: true, // (optional)
+  exclude: [
+    "/blog/.DS_Store",
+    "/blog/README.md",
+    "/blog/create",
+    "/apple-icon.png",
+    "/settings",
+    "/contact",
+    "/contact/thank-you",
+    "/privacy",
+    // "/api/get-contact-messages", // Exclude API page
+    // "/api/get-posts", // Exclude API page
+    "/contact/messages", // Exclude specific route
+    "/profile", // Exclude specific route
+  ],
+  // ...other options
+};
