ca5951a1e1f67ee543c0cda9af79488a8ed48baa
owolfdev <owolfdev@gmail.com>
2025-01-13 03:38:50 +0700

    update

--- components/mdx/inline-code.tsx
+++ components/mdx/inline-code.tsx
@@ -7,7 +7,16 @@ interface InlineCodeProps {
 
 const InlineCode: React.FC<InlineCodeProps> = ({ children }) => {
   return (
-    <code className="bg-gray-200 text-gray-900 dark:bg-gray-700 dark:text-gray-100 px-2 py-1 rounded text-base">
+    <code
+      className="bg-gray-200 text-gray-900 dark:bg-gray-700 dark:text-gray-100 px-2 py-1 rounded text-base"
+      style={{
+        maxWidth: "100%",
+        overflowX: "auto",
+        wordWrap: "break-word",
+        whiteSpace: "pre-wrap",
+        wordBreak: "break-word",
+      }}
+    >
       {children}
     </code>
   );
